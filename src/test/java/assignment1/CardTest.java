aa4b30c1b628f097f879eb8ebac5fbce6bc5596d
RickyLeak <ricky.leak97@gmail.com>
2018-10-04 03:57:44 -0400

    Small modification to CardTest.java made the getValue() and getSuite() their own public functions

--- src/test/java/assignment1/CardTest.java
+++ src/test/java/assignment1/CardTest.java
@@ -7,6 +7,10 @@ public class CardTest extends TestCase {
 	public void getValue() {
 		Card c = new Card();
 		assertEquals(5, c.getValue());
+	}
+	
+	public void getSuit() {
+		Card c = new Card();
 		assertEquals("S", c.getSuit());
 	}
 }
