ae6fb969dc25b8a76d9b762c1e1d06fa166d83c1
RickyLeak <ricky.leak97@gmail.com>
2018-10-09 02:02:39 -0400

    Minor fixes to Card class card class will now handle the single digit value (value = 10)

--- src/main/java/assignment1/Card.java
+++ src/main/java/assignment1/Card.java
@@ -7,8 +7,13 @@ public class Card {
 	//Constructor for card object
 	//We are to assume all inputs are valid
 	public Card(String string) {
+		if(string.length() > 2) {
+			this.value = (string.substring(1,3));
+		}else {
+			this.value = (string.substring(1,2));
+		}
 		this.suit = string.substring(0,1);
-		this.value = (string.substring(1,2));
+		
 	}
 	
 	//Getter for card value
