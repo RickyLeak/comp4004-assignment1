867736b66df3c1d5b5d2da68b662db75a8845689
RickyLeak <ricky.leak97@gmail.com>
2018-10-09 06:59:16 -0400

    JUnit_Test #9: Success implemented the game class and the two getter methods for both hands

--- src/test/java/assignment1/Game.java
+++ src/test/java/assignment1/Game.java
@@ -6,6 +6,23 @@ public class Game {
 	private Hand extraCards;
 	
 	public Game(String s) {
-		//initialize some stuff
+		String [] stringChunks = s.split (" ", 11);
+		String tail = stringChunks.length == 11 ? stringChunks [10] : null;
+		
+		String str_handToBeat = stringChunks[0] + " " + stringChunks[1] + " " + stringChunks[2] + " " + stringChunks[3] + " " + stringChunks[4];
+		handToBeat = new Hand(str_handToBeat);
+		
+		String str_handAIP = stringChunks[5] + " " + stringChunks[6] + " " + stringChunks[7] + " " + stringChunks[8] + " " + stringChunks[9];
+		handAIP = new Hand(str_handAIP);
+		
+		extraCards = new Hand(stringChunks[stringChunks.length-1]);
+	}
+	
+	public Hand getHandAIP() {
+		return handAIP;
+	}
+	
+	public Hand getHandtoBeat() {
+		return handToBeat;
 	}
 }
--- src/test/java/assignment1/GameTest.java
+++ src/test/java/assignment1/GameTest.java
@@ -24,6 +24,9 @@ public class GameTest {
 	public void test() throws FileNotFoundException {
 		setUpGame();
 		Game game = new Game(input);
+		System.out.println("Welcome to the poker game application!");
+		
+		System.out.println("AIP's Hand : " + game.getHandAIP());
+		System.out.println("Hand to Defeat: " + game.getHandtoBeat());
 	}
-
 }
