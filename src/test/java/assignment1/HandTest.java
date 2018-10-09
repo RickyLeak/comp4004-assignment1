636e9ca5e938a8d5da27282de27547004eaa6375
RickyLeak <ricky.leak97@gmail.com>
2018-10-09 07:59:40 -0400

    Cleaned up HandTest.java added the @Before section to prepare the test environment and removed the repeated code

--- src/test/java/assignment1/HandTest.java
+++ src/test/java/assignment1/HandTest.java
@@ -4,105 +4,77 @@ import static org.junit.Assert.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class HandTest {
 	public String input = "";
+	public Hand hand;
 	
-	private void getInput() throws FileNotFoundException {
+	@Before
+	public void getInput() throws FileNotFoundException {
 		Scanner scanner = new Scanner(new File("src\\test\\resources\\input.txt"));
 		String inputFile = scanner.useDelimiter("\\Z").next();
 		scanner.close();
 		
 		input = inputFile;
+		hand = new Hand(input);
 	}
 	
 	@Test
 	public void createHand() throws FileNotFoundException {
-		getInput();
-
-		Hand hand = new Hand(input);
 		assertTrue(hand.getCards().size() == 5);
 		System.out.println(hand.toString());
 	}
 	
 	@Test
 	public void hasPairTest() throws FileNotFoundException {
-		getInput();
-		
-		Hand hand = new Hand(input);
 		assertTrue(hand.hasPair());
 	}
 	
 	@Test
 	public void hasTripleTest() throws FileNotFoundException{
-		getInput();
-		
-		Hand hand = new Hand(input);
 		assertTrue(hand.hasTriple());
 	}
 	
 	@Test
 	public void hasFullHouseTest() throws FileNotFoundException{
-		getInput();
-		
-		Hand hand = new Hand(input);
 		assertTrue(hand.hasFullHouse());
 	}
 	
 	@Test
 	public void hasFourOfAKindTest() throws FileNotFoundException {
-		getInput();
-		
-		Hand hand = new Hand(input);
 		assertTrue(hand.hasFourOfAKind());
 	}
 	
 	@Test
 	public void hasDoublePairTest() throws FileNotFoundException {
-		getInput();
-		
-		Hand hand = new Hand(input);
 		assertTrue(hand.hasDoublePair());
 	}
 
 	@Test
 	public void getHighestCardTest() throws FileNotFoundException {
-		getInput();
-		
-		Hand hand = new Hand(input);
 		assertEquals("A", hand.getHighestCard());
 	}
 	
 	@Test
 	public void hasStraight() throws FileNotFoundException {
-		getInput();
-		
-		Hand hand = new Hand(input);
 		assertTrue(hand.hasStraight());
 	}
 	
 	@Test
 	public void hasFlushTest() throws FileNotFoundException{
-		getInput();
-		
-		Hand hand = new Hand(input);
 		assertTrue(hand.hasFlush());
 	}
 	
 	@Test
 	public void hasStraightFlushTest() throws FileNotFoundException{
-		getInput();
-		
-		Hand hand = new Hand(input);
 		assertTrue(hand.hasStraightFlush());
 	}
 	
 	@Test
 	public void hasRoyalFlushTest() throws FileNotFoundException{
-		getInput();
-		
-		Hand hand = new Hand(input);
 		assertTrue(hand.hasRoyalFlush());
 	}
 }
